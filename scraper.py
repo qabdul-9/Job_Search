cb34937018ab4e4585cb2812fcbe1965ab363f6c
Qadir Abdulahad <qabdul1@xula.edu>
2025-10-16 22:02:26 -0500

    Added "pretty welcome message" and downloaded beautifulsoup4 and scraping mission statement

--- scraper.py
+++ scraper.py
@@ -1,6 +1,28 @@
-def main():
-    print("\n\nHello, World from scraper.py!\n\n")
+import requests
+from bs4 import BeautifulSoup
 
+def main():
+
+    print('''
+__        __   _                          _ 
+\ \      / /__| | ___ ___  _ __ ___   ___| |
+ \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ |
+  \ V  V /  __/ | (_| (_) | | | | | |  __/_|
+   \_/\_/ \___|_|\___\___/|_| |_| |_|\___(_)
+        ''')
+
+    url = "https://www.xula.edu/about/mission-values.html"
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
+    response = requests.get(url, headers=headers).text
+    # response = requests.get(url)
+    soup = BeautifulSoup(response, 'html.parser')
+
+    # Extract all links
+    for link in soup.find_all('a'):
+        print(link.get('href'))
+    print(soup.div.text)
 
 if __name__ == "__main__":
     main()
+
